08e1c5e8266ccb6e6785c432d5bac07983cae5b0
thuanisuka <117148244+thuanisuka@users.noreply.github.com>
2024-01-28 20:40:15 +0700

    complete login, register

--- app/src/main/java/com/mobilers/the_little_racetrack_mobile/RegisterActivity.java
+++ app/src/main/java/com/mobilers/the_little_racetrack_mobile/RegisterActivity.java
@@ -39,38 +39,68 @@ public class RegisterActivity extends AppCompatActivity {
                 EditText txtPassword;
                 EditText txtcfPassword;
                 txtUsername = findViewById(R.id.username_register);
-                TextView txterruser = findViewById(R.id.error_user_regis);
+                String username = txtUsername.getText().toString();
+                TextView erruser = findViewById(R.id.error_user_regis);
 
                 txtPassword = findViewById(R.id.password_register);
-                TextView txterrpassword = findViewById(R.id.error_password_regis);
+                String password = txtPassword.getText().toString();
+                TextView errpassword = findViewById(R.id.error_password_regis);
 
                 txtcfPassword = findViewById(R.id.confirm_password);
-                TextView txterrcfpassword = findViewById(R.id.error_comfim_password_regis);
+                String cfpassword = txtcfPassword.getText().toString();
+                TextView errcfpassword = findViewById(R.id.error_comfim_password_regis);
 
                 UserService userService = new UserService(getApplicationContext());
 
-                if (!txtcfPassword.getText().toString().equals(txtPassword.getText().toString())){
-                    txterrcfpassword.setVisibility(View.VISIBLE);
-                    txterrcfpassword.setText("passwords do not match");
+                boolean conditionsMet = true;
 
-                    txterrpassword.setVisibility(View.VISIBLE);
-                    txterrpassword.setText("passwords do not match");
+                if (username.length() == 0) {
+                    erruser.setVisibility(View.VISIBLE);
+                    erruser.setText("cannot be left blank");
+                    conditionsMet = false;
                 }
-                else {
-               boolean checkuserExists= userService.userExists(txtUsername.getText().toString());
-                if (!checkuserExists){
-                    txterruser.setVisibility(View.INVISIBLE);
-                    userService.register(txtUsername.getText().toString(),
-                            txtPassword.getText().toString());
-//                    globalData.setCurrentUser(txtUsername.getText().toString());
-                    Intent intent = new Intent(RegisterActivity.this,
-                            MainActivity.class);
-                    startActivity(intent);
-                }else {
-                    txterruser.setVisibility(View.VISIBLE);
-                    txterruser.setText("User is exists!!!");
+                if (username.length() < 4 && username.length() !=0) {
+                    erruser.setVisibility(View.VISIBLE);
+                    erruser.setText("username must have at least 4 characters");
+                    conditionsMet = false;
                 }
+                if (password.length() == 0) {
+                    errpassword.setVisibility(View.VISIBLE);
+                    errpassword.setText("cannot be left blank");
+                    conditionsMet = false;
                 }
+                if (password.length() < 6 && password.length()!=0) {
+                    errpassword.setVisibility(View.VISIBLE);
+                    errpassword.setText("password must have at least 6 characters");
+                    conditionsMet = false;
+                }
+                if (cfpassword.length() == 0) {
+                    errcfpassword.setVisibility(View.VISIBLE);
+                    errcfpassword.setText("cannot be left blank");
+                    conditionsMet = false;
+                }
+
+                if (!password.equals(cfpassword) && password.length() > 0 && cfpassword.length() > 0) {
+                    errcfpassword.setVisibility(View.VISIBLE);
+                    errcfpassword.setText("passwords do not match");
+                    errpassword.setVisibility(View.VISIBLE);
+                    errpassword.setText("passwords do not match");
+                    conditionsMet = false;
+                }
+
+                if (conditionsMet) {
+                    boolean checkuserExists = userService.userExists(username);
+                    if (!checkuserExists) {
+                        erruser.setVisibility(View.INVISIBLE);
+                        userService.register(username, password);
+                        // globalData.setCurrentUser(txtUsername.getText().toString());
+                        finish();
+                    } else {
+                        erruser.setVisibility(View.VISIBLE);
+                        erruser.setText("User already exists!");
+                    }
+                }
+
             }
         });
 
